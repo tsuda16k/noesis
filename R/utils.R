1b24a32e8364bcfdff6b5748a6b98c98a9aaeb23
tsuda16k <tsuda16k@gmail.com>
2020-04-30 03:53:42 +0900

    Add imports

---
+++ R/utils.R
@@ -0,0 +1,13 @@
+
+
+#' @importFrom utils file_test
+#' @importFrom graphics axis plot rasterImage layout lines plot.new plot.window title abline polygon
+#' @importFrom png readPNG writePNG
+#' @importFrom jpeg writeJPEG readJPEG
+#' @importFrom readbitmap read.bitmap
+#' @importFrom methods is
+#' @importFrom stringr str_match str_split str_sub
+#' @importFrom magrittr "%>%"
+NULL
+
+
